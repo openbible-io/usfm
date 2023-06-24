9ac3e554e257e0a626f00ce6c0d3bcf06610154e
thesmartwon <thesmartwon@pm.me>
2023-06-24 16:17:22 -0400

    cleanup tokenizer

--- build.zig
+++ build.zig
@@ -1,39 +1,38 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-	const target = b.standardTargetOptions(.{});
-	const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-	const lib = b.addStaticLibrary(.{
-		.name = "usfm",
-		.root_source_file = .{ .path = "src/lib.zig" },
-		.target = target,
-		.optimize = optimize,
-	});
-	b.installArtifact(lib);
+    const lib = b.addStaticLibrary(.{
+        .name = "usfm",
+        .root_source_file = .{ .path = "src/lib.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(lib);
 
-	const exe = b.addExecutable(.{
-		.name = "usfm",
-		.root_source_file = .{ .path = "src/main.zig" },
-		.target = target,
-		.optimize = optimize,
-	});
-	b.installArtifact(exe);
+    const exe = b.addExecutable(.{
+        .name = "usfm",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    b.installArtifact(exe);
 
-	const run_cmd = b.addRunArtifact(exe);
-	run_cmd.step.dependOn(b.getInstallStep());
-	if (b.args) |args| {
-		run_cmd.addArgs(args);
-	}
-	const run_step = b.step("run", "Run the app");
-	run_step.dependOn(&run_cmd.step);
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| run_cmd.addArgs(args);
 
-	const main_tests = b.addTest(.{
-		.root_source_file = .{ .path = "src/lib.zig" },
-		.target = target,
-		.optimize = optimize,
-	});
-	const run_main_tests = b.addRunArtifact(main_tests);
-	const test_step = b.step("test", "Run library tests");
-	test_step.dependOn(&run_main_tests.step);
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/lib.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_main_tests = b.addRunArtifact(main_tests);
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&run_main_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -2,14 +2,13 @@ const std = @import("std");
 const lexer = @import("./lib.zig").lexer;
 
 pub fn main() !void {
-	var file = try std.fs.cwd().openFile("../en_ult/01-GEN.usfm", .{});
-	defer file.close();
-	var reader = file.reader();
-	var lex = try lexer(std.heap.page_allocator, reader);
-	defer lex.deinit();
+    var file = try std.fs.cwd().openFile("../en_ult/01-GEN.usfm", .{});
+    defer file.close();
+    var reader = file.reader();
+    var lex = try lexer(std.heap.page_allocator, reader);
+    defer lex.deinit();
 
-	var tok = try lex.nextToken();
-	while (tok != null) {
-		tok = try lex.nextToken();
-	}
+    while (try lex.nextToken()) |tok| {
+        _ = tok;
+    }
 }
